fd83bedd7298ac37d9834a5cb62e5bf9adf43fb1
Danilo Caetano <danilo.lopz@gmail.com>
2019-03-05 17:09:07 -0300

    Initial commit

---
+++ src/components/Banner/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Banner.css';
+
+class Banner extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React Architecture Boilerplate</h1>
+        <h3>React architecture for front-end projects</h3>
+        <p><a href="https://danilocaetano.com">Danilocaetano.com</a></p>
+      </div>
+    );
+  }
+}
+
+export default Banner;
