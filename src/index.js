576214cfeabfc7c721797b389e214ba469f14c49
Danilo Caetano <danilo.lopz@gmail.com>
2019-03-06 19:56:27 -0300

    Router configs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Banner from './components/Banner';
+// Router
+import Router from './router';
 
 // service worker
 //import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Banner />, document.getElementById('root'));
+ReactDOM.render(<Router />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,17 @@
 // React Router
-// Import Routes
-import routes from './routes';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// Import Components
+import Banner from '../components/Banner';
+
+// Create Routes
+const Router = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Banner} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Router;
